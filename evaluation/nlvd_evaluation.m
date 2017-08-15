b866af610bca4209610e7fd4d143cc6ba5f71a4b
Byungsu Min <kylemin@deeplearn12.eecs.umich.edu>
2017-08-15 04:07:33 -0400

    modify to test and evaluate dbnet on flickr30k

--- evaluation/nlvd_evaluation.m
+++ evaluation/nlvd_evaluation.m
@@ -1,7 +1,12 @@
 function [] =  nlvd_evaluation(test_title, dataset_name, subset_name, parallel)
 
+if ~exist('test_title', 'var') || isempty(teste_title)
+    test_title = 'dbnet_vgg16';
+end
+
 if ~exist('dataset_name', 'var') || isempty(dataset_name)
-    dataset_name = 'vg_v1';
+    dataset_name = 'flickr30k';
+    %dataset_name = 'vg_v1';
 end
 
 if ~exist('subset_name', 'var') || isempty(subset_name)
@@ -9,17 +14,20 @@ if ~exist('subset_name', 'var') || isempty(subset_name)
 end
 
 if ~exist('parallel', 'var') || isempty(parallel)
-    parallel = false;
+    parallel = true;
 end
 
-root_dir = fileparts(fileparts(mfilename('fullpath')));
+%root_dir = fileparts(fileparts(mfilename('fullpath')));
+root_dir = '../';
 output_dir = fullfile(root_dir, 'results', dataset_name, test_title);
 
-if exist(fullfile(output_dir, 'level_0.txt'), 'file')
+result_file = 'level_0_0.txt'
+
+if exist(fullfile(output_dir, result_file), 'file')
     fprintf('Evaluate localization based on the level_0.txt \n');
-    eval_localization(fullfile(output_dir, 'level_0.txt'), subset_name, test_title, dataset_name);
+    eval_localization(fullfile(output_dir, result_file), subset_name, test_title, dataset_name);
     fprintf('Evaluate detection based on level_0.txt \n');
-    eval_detection(fullfile(output_dir, 'level_0.txt'), 0, parallel,subset_name, test_title, dataset_name);
+    eval_detection(fullfile(output_dir, result_file), 0, parallel,subset_name, test_title, dataset_name);
 end
 
 if exist(fullfile(output_dir, 'level_1.txt'), 'file')
